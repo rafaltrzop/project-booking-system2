f62d57e9f2cb7b47e4588a7f568cdd93e19c04fe
Rafal Trzop <rafaltrzop@gmail.com>
2016-05-14 22:43:23 +0200

    Refactor gulpfile.js and add copy:js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,36 @@
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')();
 
 var sassPaths = [
   'vendor/zurb/foundation',
   'vendor/fortawesome/font-awesome'
 ];
 
-gulp.task('sass', function() {
+gulp.task('compile:sass', function() {
   return gulp.src('src/assets/stylesheets/main.scss')
     .pipe($.sass({
       includePaths: sassPaths
     })
-      .on('error', $.sass.logError))
+    .on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
     .pipe(gulp.dest('web/assets/stylesheets'));
 });
 
-gulp.task('fonts', function () {
+gulp.task('copy:fonts', function () {
   return gulp.src('vendor/fortawesome/font-awesome/fonts/fontawesome-webfont.*')
     .pipe(gulp.dest('web/assets/fonts'));
 });
 
-gulp.task('default', ['sass', 'fonts'], function() {
-  gulp.watch(['src/assets/stylesheets/**/*.scss'], ['sass']);
+gulp.task('copy:js', function () {
+  return gulp.src([
+    'node_modules/jquery/dist/jquery.min.js',
+    'src/assets/javascripts/**/*.js'
+  ]).pipe(gulp.dest('web/assets/javascripts'));
+});
+
+gulp.task('default', ['compile:sass', 'copy:fonts', 'copy:js'], function() {
+  gulp.watch(['src/assets/stylesheets/**/*.scss'], ['compile:sass']);
+  gulp.watch(['src/assets/javascripts/**/*.js'], ['copy:js']);
 });
