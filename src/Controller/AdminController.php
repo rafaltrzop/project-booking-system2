3b68ec0b64baaa9403be2ec8b6bf099a252d2a9e
Rafal Trzop <rafaltrzop@gmail.com>
2016-05-26 19:30:28 +0200

    Add edit and delete views

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -15,6 +15,10 @@ class AdminController implements ControllerProviderInterface
       ->bind('admin');
     $userController->get('/add', array($this, 'addAction'))
       ->bind('admin_add');
+    $userController->get('/edit', array($this, 'editAction'))
+      ->bind('admin_edit');
+    $userController->get('/delete', array($this, 'deleteAction'))
+      ->bind('admin_delete');
     return $userController;
   }
 
@@ -28,5 +32,17 @@ class AdminController implements ControllerProviderInterface
   {
     $view = array();
     return $app['twig']->render('Admin/add.html.twig', $view);
+  }
+
+  public function editAction(Application $app, Request $request)
+  {
+    $view = array();
+    return $app['twig']->render('Admin/edit.html.twig', $view);
+  }
+
+  public function deleteAction(Application $app, Request $request)
+  {
+    $view = array();
+    return $app['twig']->render('Admin/delete.html.twig', $view);
   }
 }
