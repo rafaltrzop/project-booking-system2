84da9c9588f484400d68f0b497dd0f19514c3277
Rafal Trzop <rafaltrzop@gmail.com>
2016-05-27 13:42:21 +0200

    Delete unnecessary use statement

--- src/Form/SubmitProjectType.php
+++ src/Form/SubmitProjectType.php
@@ -4,7 +4,6 @@ namespace Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints as Assert;
 
 class SubmitProjectType extends AbstractType
 {
