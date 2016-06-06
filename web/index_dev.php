a1df5a1eb9f4487ff10903ae06b0d3afea95d5f9
Rafal Trzop <rafaltrzop@gmail.com>
2016-06-06 21:22:19 +0200

    Refactor phpDoc and whitespace

--- web/index_dev.php
+++ web/index_dev.php
@@ -1,9 +1,6 @@
 <?php
 /**
- * Application front controller for `dev` environment.
- *
- * @copyright (c) 2016 Tomasz Chojna
- * @link http://epi.chojna.info.pl
+ * Application front controller for dev environment.
  */
 
 ini_set('error_reporting', E_ALL);
@@ -16,17 +13,14 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
   || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
   || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
-      header('HTTP/1.0 403 Forbidden');
-          exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+  header('HTTP/1.0 403 Forbidden');
+  exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
- */
+*/
 
-require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
-
-$app = require_once dirname(dirname(__FILE__)) . '/src/app.php';
-
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+$app = require_once dirname(__DIR__) . '/src/app.php';
 $app['debug'] = true;
-
-require_once dirname(dirname(__FILE__)) . '/src/controllers.php';
+require_once dirname(__DIR__) . '/src/controllers.php';
 
 $app->run();
